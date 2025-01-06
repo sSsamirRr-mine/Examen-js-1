1980c8243121e4b9e990b16582498ef9acc4d689
sSsamirRr-mine <sssamirrr4@gmail.com>
2025-01-06 23:37:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+'use strict';
+
+
+/* ""  1  ""*/
+
+let result = 5 % 2;
+alert(`5 va 2 bo'linsa ${result} `)
+
+
+
+/* ""  2  "" */
+
+// let randomNum = Math.random() * 10
+// alert(randomNum)
+
+
+
+/* ""  3  "" */
+
+// let numtofix = Math.floor(12.510);
+// alert(numtofix)
+
+
+
+/* ""  4  "" */
+
+// let lenght = "MARS IT SCHOOL".length
+// alert(lenght)
+
+
+
+/* ""  5  "" */
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("Mars It School ");
+// }
+
+
+
+/* ""  6  "" */
+
+// let harflar = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+// console.log(harflar[17].toUpperCase() + harflar[0] + harflar[11] + harflar[7] + harflar[16]);
+
+
+
+/* ""  7  "" */
+
+// let yosh = prompt("yosh")
+// if (yosh > 18) {
+//     alert("18 yoshdan kattaroqsiz")
+// } else if (yosh = 18) {
+//     alert("18 yoshsiz")
+// }else{
+//     alert("18 yoshdan kichiksiz")
+// }
+
+
+
+/* ""  8  "" */
+
+// let Ism = ["S", "a", "m", "i", "r"]
+// console.log(Ism.reverse().join(""))
+
+
+
+/* ""  9  "" */
+
+// let body = document.querySelector("body")
+// const arr = ["Ism", "Yosh"]
+// for (let i = 0; i < arr.length; i++) {
+//     let text = prompt(arr[i]).trim()
+//     let h2 = document.createElement("h2");
+//     h2.innerHTML = text;
+//     body.appendChild(h2);
+//     console.log(text);
+//     h2.style.color = "green"
+// }
+
+
+
+/* ""  10  "" */
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let o = 0
+// console.log(arr);
+// for (let o = 1; o < arr.length; o++) {
+//     if (o % 2) {
+//         console.log(arr[o], 'bu juft son');
+//     } else {
+//         console.log(arr[o] + ' ' + 'bu toq son');
+//     }
+// }
